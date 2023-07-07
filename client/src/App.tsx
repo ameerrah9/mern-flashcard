57db46e0ffd02be200020edd7028fbbb6a99d167
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-07-06 22:09:26 -0400

    Create MERN App

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='App'>
+      <p>Code will be built here</p>
+    </div>
+  );
+}
+
+export default App;
